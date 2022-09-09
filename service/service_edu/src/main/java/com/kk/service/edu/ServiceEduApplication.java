955ed27bbf53e2583dfc992635c4adfa2d312c74
krolliaa <krolliaa@163.com>
2022-09-09 13:07:43 +0800

    讲师接口 ---> ServiceEduApplication 添加 @ComponentScan 以便扫描到其它微服务模块【包括刚刚做的 MyBatisPlus 配置类】

--- service/service_edu/src/main/java/com/kk/service/edu/ServiceEduApplication.java
+++ service/service_edu/src/main/java/com/kk/service/edu/ServiceEduApplication.java
@@ -2,8 +2,10 @@ package com.kk.service.edu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.kk"})
 public class ServiceEduApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServiceEduApplication.class, args);
