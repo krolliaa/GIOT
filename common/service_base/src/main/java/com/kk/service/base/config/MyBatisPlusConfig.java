2a55319dfd9e052a8c19690b56adfd4ee08533e5
krolliaa <krolliaa@163.com>
2022-09-09 13:06:54 +0800

    讲师接口 ---> MyBatisPlusConfig 添加 MapperScan 以便扫描到各个微服务的 mapper 包

---
+++ common/service_base/src/main/java/com/kk/service/base/config/MyBatisPlusConfig.java
@@ -0,0 +1,25 @@
+package com.kk.service.base.config;
+
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+@MapperScan(basePackages = {"com.kk.service.*.mapper"})
+public class MyBatisPlusConfig {
+
+    /**
+     * 分页插件
+     * @return
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
+        mybatisPlusInterceptor.addInnerInterceptor(new PaginationInnerInterceptor());
+        return mybatisPlusInterceptor;
+    }
+}
