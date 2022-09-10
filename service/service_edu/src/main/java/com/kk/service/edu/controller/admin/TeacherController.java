0983b7ddabc42e15cfbdc5beffde70b7c666e61e
krolliaa <krolliaa@163.com>
2022-09-10 10:15:41 +0800

    Swagger 相关注解： 1. @ApiModelProperty ---> 实体类属性说明 2. @Api ---> 接口类说明 3. @ApiOperation ---> 接口说明 4. @ApiParam ---> 接口参数说明

--- service/service_edu/src/main/java/com/kk/service/edu/controller/admin/TeacherController.java
+++ service/service_edu/src/main/java/com/kk/service/edu/controller/admin/TeacherController.java
@@ -2,6 +2,9 @@ package com.kk.service.edu.controller.admin;
 
 import com.kk.service.edu.pojo.Teacher;
 import com.kk.service.edu.service.TeacherService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,6 +18,7 @@ import java.util.List;
  * @author kk
  * @since 2022-09-09
  */
+@Api(value = "讲师管理接口")
 @RestController
 @RequestMapping("/admin/edu/teacher")
 public class TeacherController {
@@ -22,13 +26,15 @@ public class TeacherController {
     @Autowired
     private TeacherService teacherService;
 
+    @ApiOperation(value = "所有讲师列表")
     @GetMapping(value = "/list")
     public List<Teacher> listAll() {
         return teacherService.list();
     }
 
+    @ApiOperation(value = "根据 id 删除讲师【逻辑删除】", notes = "根据 id 删除讲师【逻辑删除】")
     @DeleteMapping(value = "/remove/{id}")
-    public Boolean removeById(@PathVariable(value = "id") String id) {
+    public Boolean removeById(@ApiParam(value = "讲师id", required = true) @PathVariable(value = "id") String id) {
         return teacherService.removeById(id);
     }
 }
