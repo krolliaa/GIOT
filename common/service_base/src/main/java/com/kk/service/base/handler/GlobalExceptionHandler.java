a6bbfacfdf32dc36817d25103e92225ec33d5cd3
krolliaa <krolliaa@163.com>
2022-09-10 18:37:38 +0800

    讲师接口 ---> 特定异常处理

--- common/service_base/src/main/java/com/kk/service/base/handler/GlobalExceptionHandler.java
+++ common/service_base/src/main/java/com/kk/service/base/handler/GlobalExceptionHandler.java
@@ -1,9 +1,12 @@
 package com.kk.service.base.handler;
 
 import com.kk.common.result.ResultData;
+import org.springframework.jdbc.BadSqlGrammarException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.sql.SQLSyntaxErrorException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -12,5 +15,12 @@ public class GlobalExceptionHandler {
     public ResultData error(Exception exception) {
         exception.printStackTrace();
         return ResultData.error().message("全局异常统一处理");
+    }
+
+    @ExceptionHandler(value = {BadSqlGrammarException.class})
+    @ResponseBody
+    public ResultData error(BadSqlGrammarException badSqlGrammarException) {
+        badSqlGrammarException.printStackTrace();
+        return ResultData.error().message("SQL语法错误");
     }
 }
