024b94333f2460624330ed8091f3ea147013465a
krolliaa <krolliaa@163.com>
2022-09-10 18:28:24 +0800

    讲师接口 ---> 新增和修改 + 根据ID查询讲师 讲师接口 ---> 全局异常统一处理：@ControllerAdvice + @ExceptionHandler + @ResponseBody

---
+++ common/service_base/src/main/java/com/kk/service/base/handler/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.kk.service.base.handler;
+
+import com.kk.common.result.ResultData;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(Exception.class)
+    @ResponseBody
+    public ResultData error(Exception exception) {
+        exception.printStackTrace();
+        return ResultData.error();
+    }
+}
--- service/service_edu/src/main/java/com/kk/service/edu/controller/admin/TeacherController.java
+++ service/service_edu/src/main/java/com/kk/service/edu/controller/admin/TeacherController.java
@@ -29,13 +29,30 @@ public class TeacherController {
     @Autowired
     private TeacherService teacherService;
 
+    /**
+     * 获取所有讲师列表接口
+     *
+     * @return
+     */
     @ApiOperation(value = "所有讲师列表")
     @GetMapping(value = "/list")
     public ResultData listAll() {
         List<Teacher> teachers = teacherService.list();
-        return ResultData.ok().data("teachers", teachers).message("获取讲师列表成功");
+        if (teachers != null) {
+            return ResultData.ok().data("teachers", teachers).message("获取讲师列表成功");
+        } else {
+            return ResultData.error().message("数据不存在");
+        }
     }
 
+    /**
+     * 分页获取讲师列表接口
+     *
+     * @param current
+     * @param size
+     * @param teacherQueryVo
+     * @return
+     */
     @ApiOperation(value = "分页讲师列表")
     @GetMapping(value = "/list/{current}/{size}")
     public ResultData listPage(@ApiParam(value = "当前页码", required = true) @PathVariable(value = "current") Long current,
@@ -44,9 +61,17 @@ public class TeacherController {
         Page<Teacher> teacherPage = teacherService.selectPage(current, size, teacherQueryVo);
         List<Teacher> teachers = teacherPage.getRecords();
         long teacherPageTotal = teacherPage.getTotal();
+        if (teacherPage != null) {
+        }
         return ResultData.ok().message("获取分页讲师列表成功").data("total", teacherPageTotal).data("rows", teachers);
     }
 
+    /**
+     * 根据 id 删除讲师接口
+     *
+     * @param id
+     * @return
+     */
     @ApiOperation(value = "根据 id 删除讲师【逻辑删除】", notes = "根据 id 删除讲师【逻辑删除】")
     @DeleteMapping(value = "/remove/{id}")
     public ResultData removeById(@ApiParam(value = "讲师id", required = true) @PathVariable(value = "id") String id) {
@@ -55,6 +80,40 @@ public class TeacherController {
             return ResultData.ok().message("删除成功");
         } else {
             return ResultData.error().message("删除失败，数据不存在");
+        }
+    }
+
+    @ApiOperation(value = "新增讲师")
+    @PostMapping("/save")
+    public ResultData save(@ApiParam(value = "讲师对象", required = true) @RequestBody Teacher teacher) {
+        Boolean success = teacherService.save(teacher);
+        if (success) {
+            return ResultData.ok().message("保存成功");
+        } else {
+            return ResultData.error().message("保存失败");
+        }
+    }
+
+    @ApiOperation(value = "更新讲师")
+    @PutMapping(value = "/update")
+    public ResultData updateById(@ApiParam(value = "讲师对象", required = true) @RequestBody Teacher teacher) {
+        Boolean success = teacherService.updateById(teacher);
+        if (success) {
+            return ResultData.ok().message("修改成功");
+        } else {
+            return ResultData.error().message("数据不存在");
+        }
+    }
+
+    @ApiOperation(value = "根据 id 获取讲师")
+    @GetMapping(value = "/{id}")
+    public ResultData getById(@ApiParam(value = "讲师ID", required = true) @PathVariable(value = "id") String id) {
+        //这里会自动查询没有被逻辑删除的数据，666
+        Teacher teacher = teacherService.getById(id);
+        if (teacher != null) {
+            return ResultData.ok().message("查询成功").data("item", teacher);
+        } else {
+            return ResultData.error().message("数据不存在");
         }
     }
 }
